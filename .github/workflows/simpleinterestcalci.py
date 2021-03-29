fd6492ba054ab5d2e93d2a4ef5dfe07820043f20
Devesh1602 <76179606+Devesh1602@users.noreply.github.com>
2021-03-29 11:17:56 +0530

    Create simpleinterestcalci.py

---
+++ .github/workflows/simpleinterestcalci.py
@@ -0,0 +1,7 @@
+#Simple Interest Calculator (DEVESH SINGH)
+#Principal,Rate,Time
+P=float(input("Enter Principal="))            #Principal Value
+R=float(input("Enter Rate="))                 #Rate Value
+T=float(input("Enter Time="))                 #Time Value
+si=(P*R*T)/100                                #Simple Interest Formula
+print("The Simple Interest is",format(si))    #Display Result
